22ac3ade624fe4c8745ec42299af8f37175210ac
EmiiFernandez <e.fernandezmurgia@gmail.com>
2023-12-09 19:16:18 -0300

    Circuit Breaker + Retry

--- Spring-Cloud/checkout-service/checkout-service/src/main/java/com/example/checkoutservice/repository/FeignProductRepository.java
+++ Spring-Cloud/checkout-service/checkout-service/src/main/java/com/example/checkoutservice/repository/FeignProductRepository.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 @Repository
-//@FeignClient(name = "products-service") //nombre del serv al que queremos comunicarnos
-@FeignClient(name = "products", url = "http://localhost:8084") //nombre del serv al que queremos comunicarnos
+@FeignClient(name = "products-service") //nombre del serv al que queremos comunicarnos
+//@FeignClient(name = "products", url = "http://localhost:8084") //nombre del serv al que queremos comunicarnos
 //Configuración para LoadBalancer Random
 //@LoadBalancerClient(value = "products-service", configuration = LoadBalancerConfiguration.class)
 public interface FeignProductRepository {
     @RequestMapping(method = RequestMethod.GET, value="/products")
-    Product getProductById(@RequestParam String id);
+    Product getProductById(@RequestParam String id, @RequestParam Boolean throwError);
 }
--- Spring-Cloud/checkout-service/checkout-service/src/main/java/com/example/checkoutservice/service/impl/ProductService.java
+++ Spring-Cloud/checkout-service/checkout-service/src/main/java/com/example/checkoutservice/service/impl/ProductService.java
@@ -6,6 +6,7 @@ import com.example.checkoutservice.service.IProductService;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,19 @@ public class ProductService implements IProductService {
     private FeignProductRepository feignProductRepository;
 
     Logger log = LoggerFactory.getLogger(ProductService.class);
+
     public ProductService(FeignProductRepository feignProductRepository) {
         super();
         this.feignProductRepository = feignProductRepository;
     }
 
     @Override
-    @CircuitBreaker(name="product", fallbackMethod = "getProductFallbackMethod") //Config CB
+    @CircuitBreaker(name="product", fallbackMethod="getProductFallbackMethod") //Config CB
+    @Retry(name="product")
     public Product getProduct(String id) {
+        log.info("Intentando obtener el producto con ID: " + id);
 
-        return feignProductRepository.getProductById(id);
+        return feignProductRepository.getProductById(id, true);
     }
 
     //Método que se ejectuta si CB esta open
@@ -33,6 +37,6 @@ public class ProductService implements IProductService {
     public Product getProductFallbackMethod(String id, CallNotPermittedException exception) {
         log.error("Circuit Breaker en estado Open");
 
-        return new Product();
+        return new Product(); //Retorna un producto vacio
     }
 }
